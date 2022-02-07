f92c9aa8593cbf1a69128543734d92d9e49e7b9b
Kalie Kirch <kirchk0446@ad.faytechcc.edu>
2022-02-07 11:02:21 -0500

    Add project files.

---
+++ TotalSalesWin/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TotalSalesWin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.totalSalesArray = new System.Windows.Forms.ListBox();
+            this.totalSumOfSales = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // totalSalesArray
+            // 
+            this.totalSalesArray.FormattingEnabled = true;
+            this.totalSalesArray.Location = new System.Drawing.Point(155, 75);
+            this.totalSalesArray.Name = "totalSalesArray";
+            this.totalSalesArray.Size = new System.Drawing.Size(603, 355);
+            this.totalSalesArray.TabIndex = 0;
+            // 
+            // totalSumOfSales
+            // 
+            this.totalSumOfSales.AutoSize = true;
+            this.totalSumOfSales.Location = new System.Drawing.Point(389, 466);
+            this.totalSumOfSales.Name = "totalSumOfSales";
+            this.totalSumOfSales.Size = new System.Drawing.Size(31, 13);
+            this.totalSumOfSales.TabIndex = 1;
+            this.totalSumOfSales.Text = "Total";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 587);
+            this.Controls.Add(this.totalSumOfSales);
+            this.Controls.Add(this.totalSalesArray);
+            this.Name = "Form1";
+            this.Text = "totalSales";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox totalSalesArray;
+        private System.Windows.Forms.Label totalSumOfSales;
+    }
+}
+
---
+++ TotalSalesWin/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TotalSalesWin
+{   /* 
+     1/31/2022
+     CSC 153
+     Kalie Kirch
+     A listbox Windows form to calculate total sales 
+    */
+    public partial class Form1 : Form
+    {
+        decimal[] sales = { 1245.67m, 1189.55m, 1098.72m, 1456.88m, 2109.34m, 1987.55m, 1872.36m };
+        decimal total = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            totalSalesArray.Items.Clear();
+            foreach (decimal sale in sales)
+            {
+                totalSalesArray.Items.Add(sale.ToString("C"));
+                total += sale;
+
+            }
+            totalSumOfSales.Text = total.ToString("C");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
